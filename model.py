add862090283d0b0dee9cd0748d2331ee002ef89
Andrew Boney <andrew.boney@hotmail.co.uk>
2024-10-26 20:39:57 +0000

    wip

--- model.py
+++ model.py
@@ -1,9 +1,21 @@
-from transformers import AutoModelForCausalLM
+import lightning as L
 
-class AutoModelForCausalLMBestOfN(AutoModelForCausalLM):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+import torch
 
-        self.score
+from torch import nn
 
-    def 
+class RewardModelLM(L.LightningModule):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+        self.model.score = nn.Linear(self.model.config.hidden_size, 1)
+
+    def forward(self, *args, **kwargs):
+        if "output_hidden_states" in kwargs.items():
+            raise ValueError("`output_hidden_states` can't be set as hidden state outputs are required")
+        output = self.model(*args, **kwargs, output_hidden_states = True)
+
+        logits = output.hidden_states[-1][:, -1, :]
+
+        with torch.amp.autocast(self.device.type):
+            return self.model.score(logits)
